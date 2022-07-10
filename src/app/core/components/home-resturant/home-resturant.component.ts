39e7e28aa1370c6ea7b07745daba583789b02e1e
Taha Alokla <taha.aloklaa1996@gmail.com>
2022-07-10 19:24:16 +0300

    add tailwindcss to project

--- src/app/core/components/home-resturant/home-resturant.component.ts
+++ src/app/core/components/home-resturant/home-resturant.component.ts
@@ -3,18 +3,21 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 import { NumericValueType, RxwebValidators } from '@rxweb/reactive-form-validators';
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import { ReservationsRestaurant } from 'src/app/shared/interfaces/Reservations-Restaurant.interface';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home-resturant',
   templateUrl: './home-resturant.component.html',
   styleUrls: ['./home-resturant.component.scss']
 })
 export class HomeResturantComponent implements OnInit {
-
+  rest_id:string=''
 
   minDate = new Date(new Date().setDate(new Date().getDate()));
   // max date after 4 month
   maxDate = new Date(new Date().setDate(new Date().getDate() + 120))
-  constructor(private fb: FormBuilder, private RestaurantHomePageService: RestaurantHomePageService) { }
+  constructor(private fb: FormBuilder, private RestaurantHomePageService: RestaurantHomePageService, private activeRouter: ActivatedRoute) { }
 
   registerReservation = this.fb.group({
     name: ['', [Validators.required]],
@@ -23,18 +26,90 @@ export class HomeResturantComponent implements OnInit {
     dateOfReservation: ['', [Validators.required]],
     note: ['', []],
   })
+  // TODO : 6/7/2022
   // "rest_id": "62bcecc3aa5f2d5599b2532d",
   // "checkSpam": "5"
 
   getError(controlName: string) {
     return this.registerReservation.get(controlName)?.invalid && (this.registerReservation.get(controlName)?.touched || this.registerReservation.get(controlName)?.dirty)
   }
-
+  get numberTable() {
+    return this.registerReservation.get('numberTable');
+  }
+  get phone() {
+    return this.registerReservation.get('phone');
+  }
   ngOnInit(): void {
-    const id = uuidv4();
+    // const id = uuidv4();
     //  TODO when refresh browser change value | so be should save that local storage and check if existing dot need return value restaurant
     // todo : i think generation uuid when using postSend data not ngonit
-    console.log(" uuid.get()", id);
+    // console.log(" uuid.get()", id);
+    this.activeRouter.paramMap.subscribe(param => {
+      this.rest_id = param.get('id');
+      // console.log("this.rest_id", this.rest_id);
+       });
   }
+  registerReservationSubmit() {
+    let checkSpam: string = ''
+    if (this.RestaurantHomePageService.getUUID()) {
+      console.log("this.RestaurantHomePageService.getUUID()", this.RestaurantHomePageService.getUUID());
+      checkSpam = this.RestaurantHomePageService.getUUID()
+      console.log("checkSpam", checkSpam);
+    } else {
+      const id = uuidv4();
+      this.RestaurantHomePageService.saveUUID(id)
+      checkSpam = id
+      console.log(" else checkSpam", checkSpam);
+    }
+    let registerReservationData: ReservationsRestaurant = {
+      name: this.registerReservation.value.name,
+      phone: this.registerReservation.value.phone as string,
+      numberTable: +this.registerReservation.value.numberTable,
+      dateOfReservation: this.registerReservation.value.dateOfReservation,
+      note: this.registerReservation.value.note,
+      checkSpam: checkSpam,
+      rest_id: this.rest_id
+    }
+    // console.log(registerReservationData);
+
+    this.RestaurantHomePageService.postReservations_restaurants(registerReservationData,this.rest_id ).subscribe({
+      next: (data) => {
+        // console.log(data);
+        this.registerReservation.reset()
+        Swal.fire(
+         {
+          title:'تم تثبيت الحجز',
+          icon: 'success',
+          showCloseButton: false,
+          showConfirmButton: true,
+          showCancelButton:false,
+          focusConfirm: true,
+          confirmButtonText:'إغلاق',
+          timer: 4500,
+          confirmButtonColor:'#d33'
+         }
+        )
+      }, error: (err) => {
+        // console.log(err);
+        this.registerReservation.reset()
+        Swal.fire(
+          {
+           title:'هناك حجز مسبق قمت به',
+           icon: 'error',
+           showCloseButton: false,
+          showConfirmButton: true,
+          showCancelButton:false,
+          focusConfirm: true,
+          confirmButtonText:'إغلاق',
+          timer: 4500,
+          confirmButtonColor:'#d33'
+          }
+         )
+
+      }
+    })
+
+  }
+
 
 }
--- src/app/shared/services/restaurant-home-page.service.ts
+++ src/app/shared/services/restaurant-home-page.service.ts
@@ -12,11 +12,12 @@ const uuid=''
 export class RestaurantHomePageService {
   // /api/restaurants/62bcecc3aa5f2d5599b2532d/reservations/create
 private readonly idRestaurant:string=''
-private readonly reservations_restaurants_url:string=`${baseUrl}/api/restaurants/${this.idRestaurant}/reservations/create`
+// private readonly reservations_restaurants_url:string=`${baseUrl}/api/restaurants/${this.idRestaurant}/reservations/create`
   constructor(private http:HttpClient) { }
 
-  postReservations_restaurants(data:ReservationsRestaurant):Observable<any>{
-    return this.http.post(this.reservations_restaurants_url,data)
+  postReservations_restaurants(data:ReservationsRestaurant ,id_rest:string):Observable<any>{
+  let  reservations_restaurants_url=`${baseUrl}/api/restaurants/${id_rest}/reservations/create`
+    return this.http.post(reservations_restaurants_url,data)
   }
 
   public saveUUID(uuidVal){
