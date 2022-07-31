061938927f2261e98d27c88e06d9cc64afc4f858
Taha Alokla <taha.aloklaa1996@gmail.com>
2022-07-31 12:09:55 +0300

    update

---
+++ src/app/Resturant-dashboard/pipes/time12hours.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { Time12hoursPipe } from './time12hours.pipe';
+
+describe('Time12hoursPipe', () => {
+  it('create an instance', () => {
+    const pipe = new Time12hoursPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
